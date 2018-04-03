602923bd6876a823fee028ae793c8a9ab3e46bde
noahahillman <37415078+noahahillman@users.noreply.github.com>
2018-04-02 22:01:29 -0400

    Simple a star (#15)
    * first push with pathfinder, path, and astarnodes. There is no hashmap of nodes and edges, still must be addded
    
    * this time adding pathfinder, astarnode, and path with the actual file changes added correctly
    
    * starting writing text directions based on turn angles
    
    * first push with pathfinder, path, and astarnodes. There is no hashmap of nodes and edges, still must be addded
    
    * this time adding pathfinder, astarnode, and path with the actual file changes added correctly
    
    * starting writing text directions based on turn angles
    
    * functioning a-star with database reading capabilities
    
    * obviously I can't github if I diverged my own branch somehow, attemping to fix
    
    * adding checks to make sure nodes are pulled from db correctly
    
    * cleaning up code by removing print statements
    
    * removing main tests for db

---
+++ src/main/java/pathfind/Path.java
@@ -0,0 +1,51 @@
+package pathfind;
+
+import java.util.ArrayList;
+
+/**
+ * Path.java
+ * Holds path information from A-star and calculates text directions
+ * Author: Noah Hillman
+ * Date: April 2, 2018
+ */
+
+public class Path {
+    ArrayList<AStarNode> a_star_node_path = new ArrayList<>();
+
+    public Path(){
+    }
+
+
+    public String getPathDirections(){
+        String path = "";
+        path += "path from: " + a_star_node_path.get(0).getShortName() + " to: " + a_star_node_path.get(a_star_node_path.size()-1).getShortName() + System.lineSeparator();
+        for (int i = 1; i < (a_star_node_path.size()-1) ; i++) {
+        }
+        //TODO add path descriptions
+        return path;
+    }
+
+    private String calcTurn(AStarNode current_node, AStarNode next_node){
+        int previous_x = current_node.getParent().getX_coord();
+        int previous_y = current_node.getParent().getY_coord();
+        int current_x = current_node.getX_coord();
+        int current_y = current_node.getY_coord();
+        int next_x = next_node.getX_coord();
+        int next_y = next_node.getY_coord();
+
+        double previous_angle = angle(previous_x, previous_y, current_x, current_y);
+        double new_angle = angle(current_x, current_y, next_x, next_y);
+        double angle = new_angle-previous_angle;
+        if(angle <= -30){
+            return "turn left";
+        } else if(angle >= 30){
+            return "turn right";
+        } else {
+            return "continue straight";
+        }
+    }
+
+    private double angle(int x1, int y1, int x2, int y2){
+        return Math.toDegrees(Math.atan2(y2 - y1, x2 - x1));
+    }
+}
