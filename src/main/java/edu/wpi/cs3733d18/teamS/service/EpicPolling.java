253898fa1334de7aaf1158d2f8ac8b1ab7569b02
Mitch <gainesmitch2@gmail.com>
2018-04-23 01:24:04 -0400

    formatting description

--- src/main/java/edu/wpi/cs3733d18/teamS/service/EpicPolling.java
+++ src/main/java/edu/wpi/cs3733d18/teamS/service/EpicPolling.java
@@ -1,9 +1,16 @@
 package edu.wpi.cs3733d18.teamS.service;
 
 public class EpicPolling implements Runnable {
+    private Thread t;
 
     @Override
     public void run() {
+        //look for tasks on server and add them to db as service requests
+    }
 
+    public void start(){
+        if(t == null){
+            t.start();
+        }
     }
 }
