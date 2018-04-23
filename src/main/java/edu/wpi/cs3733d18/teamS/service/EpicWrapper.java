ca74376681d0768c96f6aaaa0b57e4ce736253af
Mitch <gainesmitch2@gmail.com>
2018-04-23 01:24:04 -0400

    formatting description

--- src/main/java/edu/wpi/cs3733d18/teamS/service/EpicWrapper.java
+++ src/main/java/edu/wpi/cs3733d18/teamS/service/EpicWrapper.java
@@ -8,11 +8,23 @@ public class EpicWrapper {
 
     private FhirContext fhir_ctx = FhirContext.forDstu3();
 
-    public void createEpicService(String description){
+    public void createEpicService(ServiceRequest sr){
+        StringBuilder description = new StringBuilder();
         Task hl7_task = new Task();
+
+        description.append("At ")
+                .append(sr.requestedDate)
+                .append(", ")
+                .append(sr.getRequester())
+                .append(" requested ")
+                .append(sr.getTitle())
+                .append(" in ")
+                .append(sr.getLocation().getLongName())
+                .append(". ")
+                .append(sr.getDescription());
+
         hl7_task.setStatus(Task.TaskStatus.REQUESTED);
-        hl7_task.setDescription(description);
-        hl7_task.setRequester(new Reference());
+        hl7_task.setDescription(sr.getDescription());
     }
 /*
     public ServiceRequest getEpicServiceRequest(){
