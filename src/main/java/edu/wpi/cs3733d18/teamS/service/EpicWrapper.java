8ab041758e158ec8274cb77c5d26e085bb16c152
Mitch <gainesmitch2@gmail.com>
2018-04-24 10:33:37 -0400

    more epic

--- src/main/java/edu/wpi/cs3733d18/teamS/service/EpicWrapper.java
+++ src/main/java/edu/wpi/cs3733d18/teamS/service/EpicWrapper.java
@@ -1,16 +1,22 @@
 package edu.wpi.cs3733d18.teamS.service;
 
 import ca.uhn.fhir.context.FhirContext;
-import org.hl7.fhir.dstu3.model.Reference;
 import org.hl7.fhir.dstu3.model.Task;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 
 public class EpicWrapper {
 
-    private FhirContext fhir_ctx = FhirContext.forDstu3();
+    private static FhirContext fhir_ctx = FhirContext.forDstu3();
 
-    public void createEpicService(ServiceRequest sr){
+    public static byte[] createEpicService(ServiceRequest sr){
+
         StringBuilder description = new StringBuilder();
         Task hl7_task = new Task();
+        byte[] hl7_task_b = new byte[10];
 
         description.append("At ")
                 .append(sr.requestedDate)
@@ -24,6 +30,25 @@ public class EpicWrapper {
                 .append(sr.getDescription());
 
         hl7_task.setStatus(Task.TaskStatus.REQUESTED);
-        hl7_task.setDescription(sr.getDescription());
+        hl7_task.setDescription(description.toString());
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = null;
+        try{
+            out = new ObjectOutputStream(bos);
+            out.writeObject(hl7_task);
+            out.flush();
+            hl7_task_b = bos.toByteArray();
+        } catch(IOException ex) {
+            System.out.println("Unable to make byte array out of HL7 Object");
+        } finally {
+            try{
+                bos.close();
+            } catch (IOException ex) {
+                System.out.println("Unable to clse ByteArrayOutputStream");
+            }
+        }
+
+        return hl7_task_b;
     }
 }
